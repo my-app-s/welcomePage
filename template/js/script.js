657a9177de6d6853a4d34387b16c27d987514b86
m-app-s <connect.to.line.0@gmail.com>
2025-04-26 17:54:44 +0500

    Edit js code

--- template/js/script.js
+++ template/js/script.js
@@ -6,9 +6,17 @@ const clickableDivGithub = document.getElementById("div-with-github");
 const clickableDivLinkedin = document.getElementById("div-with-linkedin");
 const clickableDivHH = document.getElementById("div-with-hh");
 
-clickableDivGithub.addEventListener("click", () => navigateTo("https://github.com/my-app-s"));
-clickableDivLinkedin.addEventListener("click", () => navigateTo("https://www.linkedin.com/in/rustem-m-692916334"));
-clickableDivHH.addEventListener("click", () => navigateTo("https://hh.ru/resume/c88917e1ff0e3ec76d0039ed1f695343343372"));
+function handleNavigation(event, url) {
+    if (event.button === 0) { // Левая кнопка мыши
+        navigateTo(url);
+    } else if (event.button === 1) { // Средняя кнопка мыши
+        window.open(url, "_blank"); // Открывает в новой вкладке
+    }
+}
+
+clickableDivGithub.addEventListener("mousedown", (event) => handleNavigation(event, "https://github.com/my-app-s"));
+clickableDivLinkedin.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.linkedin.com/in/rustem-m-692916334"));
+clickableDivHH.addEventListener("mousedown", (event) => handleNavigation(event, "https://hh.ru/resume/c88917e1ff0e3ec76d0039ed1f695343343372"));
 
 document.addEventListener("contextmenu", function (e) {
     e.preventDefault(); // Отключает контекстное меню
