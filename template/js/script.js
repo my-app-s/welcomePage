12447ce32f7ca50af80ccada52e3f62bcf7225b3
m-app-s <connect.to.line.0@gmail.com>
2025-05-04 02:42:01 +0500

    Update file with script

--- template/js/script.js
+++ template/js/script.js
@@ -13,28 +13,28 @@ function handleNavigation(event, url) {
 const clickableDivGithub = document.getElementById("div-with-github");
 const clickableDivLinkedin = document.getElementById("div-with-linkedin");
 const clickableDivHH = document.getElementById("div-with-hh");
-const clickableSetr1 = document.getElementById("div-body-setrificate-container-item-1");
-const clickableSetr2 = document.getElementById("div-body-setrificate-container-item-2");
-const clickableSetr3 = document.getElementById("div-body-setrificate-container-item-3");
-const clickableSetr4 = document.getElementById("div-body-setrificate-container-item-4");
-const clickableSetr5 = document.getElementById("div-body-setrificate-container-item-5");
-const clickableSetr6 = document.getElementById("div-body-setrificate-container-item-6");
-const clickableSetr7 = document.getElementById("div-body-setrificate-container-item-7");
-const clickableSetr8 = document.getElementById("div-body-setrificate-container-item-8");
-const clickableSetr9 = document.getElementById("div-body-setrificate-container-item-9");
+const clickableCetr1 = document.getElementById("div-body-cetrificate-container-item-1");
+const clickableCetr2 = document.getElementById("div-body-cetrificate-container-item-2");
+const clickableCetr3 = document.getElementById("div-body-cetrificate-container-item-3");
+const clickableCetr4 = document.getElementById("div-body-cetrificate-container-item-4");
+const clickableCetr5 = document.getElementById("div-body-cetrificate-container-item-5");
+const clickableCetr6 = document.getElementById("div-body-cetrificate-container-item-6");
+const clickableCetr7 = document.getElementById("div-body-cetrificate-container-item-7");
+const clickableCetr8 = document.getElementById("div-body-cetrificate-container-item-8");
+const clickableCetr9 = document.getElementById("div-body-cetrificate-container-item-9");
 
 clickableDivGithub.addEventListener("mousedown", (event) => handleNavigation(event, "https://github.com/my-app-s"));
 clickableDivLinkedin.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.linkedin.com/in/rustem-m-692916334"));
 clickableDivHH.addEventListener("mousedown", (event) => handleNavigation(event, "https://hh.ru/resume/c88917e1ff0e3ec76d0039ed1f695343343372"));
-clickableSetr1.addEventListener("mousedown", (event) => handleNavigation(event, "https://stepik.org/cert/1028496?lang=en"));
-clickableSetr2.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-NDOUR87X"));
-clickableSetr3.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-BQ6T9O18"));
-clickableSetr4.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-KIWHL5L7"));
-clickableSetr5.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-ETRVSDXT"));
-clickableSetr6.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-PNPTUELV"));
-clickableSetr7.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-K5PCIELQ"));
-clickableSetr8.addEventListener("mousedown", (event) => handleNavigation(event, "https://stepik.org/cert/2532641?lang=en"));
-clickableSetr9.addEventListener("mousedown", (event) => handleNavigation(event, "https://stepik.org/cert/2827910?lang=en"));
+clickableCetr1.addEventListener("mousedown", (event) => handleNavigation(event, "https://stepik.org/cert/1028496?lang=en"));
+clickableCetr2.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-NDOUR87X"));
+clickableCetr3.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-BQ6T9O18"));
+clickableCetr4.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-KIWHL5L7"));
+clickableCetr5.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-ETRVSDXT"));
+clickableCetr6.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-PNPTUELV"));
+clickableCetr7.addEventListener("mousedown", (event) => handleNavigation(event, "https://www.sololearn.com/en/certificates/CT-K5PCIELQ"));
+clickableCetr8.addEventListener("mousedown", (event) => handleNavigation(event, "https://stepik.org/cert/2532641?lang=en"));
+clickableCetr9.addEventListener("mousedown", (event) => handleNavigation(event, "https://stepik.org/cert/2827910?lang=en"));
 
 document.addEventListener("contextmenu", function (e) {
     e.preventDefault(); // Отключает контекстное меню
